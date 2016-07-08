27293792f82e82276c88973b50e8b384e813296e
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-07-07 21:23:03 -0700

    Add basic travis-ci configuration and test script

---
+++ run_tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SUCCESS_RATE=80
+printf 'percentage of runs that succeed: %s%%\n' "$SUCCESS_RATE"
+
+WORK_DURATION=45
+printf 'pretending to work for %i seconds... ' "$WORK_DURATION"
+sleep $WORK_DURATION
+printf 'done\n'
+
+PASSING_TESTS=$(( (RANDOM % 100)  + 1 ))
+printf 'simulated test result status... '
+if [[ $PASSING_TESTS -le $SUCCESS_RATE ]]; then
+	printf 'SUCCESS\n'
+	exit 0
+else
+	printf 'FAILURE\n'
+	exit 1
+fi
